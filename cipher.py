fcc6ae6070d2df4d0d9bab7f3ae299edfe3ffcb8
Javier Morales Casas <javier.moralescasas@estudiante.uam.es>
2020-04-06 00:00:51 +0200

    Firma de archivo

--- cipher.py
+++ cipher.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 # Modulo de cifrado, firma de archivos y generacion de claves
-from Crypto.PublicKey import RSA
 import os
-import logging
 import config
+import sys
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature import pkcs1_15
 
 
 def gen_id():
@@ -15,14 +17,43 @@ def gen_id():
     if not os.path.isfile(filepath_priv) or not os.path.isfile(filepath_publ):
         key = RSA.generate(2048)
         private_key = key.export_key()
-        file_out = open(filepath_priv, "wb")
-        file_out.write(private_key)
-        file_out.close()
+        with open(filepath_priv, "wb") as file_out:
+            file_out.write(private_key)
 
         public_key = key.publickey().export_key()
-        file_out = open(filepath_publ, "wb")
-        file_out.write(public_key)
-        file_out.close()
+        with open(filepath_publ, "wb") as file_out:
+            file_out.write(public_key)
         print("-> Generando par de claves RSA de 2048 bits...OK")
     else:
         print("-> Generando par de claves RSA de 2048 bits...Ya existen!")
+
+
+def sign(fichero):
+    """Firma el fichero recibido, guardandolo en un archivo con un prefijo seguido del nombre.
+    """
+    # Direcciones a usar
+    filepath = os.path.abspath(os.path.join(config.FILES_DIR, fichero))
+    signed_fp = os.path.abspath(os.path.join(config.FILES_DIR, config.SIGNED_PREFIX + fichero))
+    priv_key_path = os.path.abspath(os.path.join(config.KEYS_DIR, 'private.pem'))
+    # Obtener clave privada propia
+    private_key = RSA.import_key(open(priv_key_path).read())
+    # Obtener hash del fichero, cifrarlo con la clave privada y guardar la firma
+    if os.path.isfile(filepath):
+        with open(filepath, "rb") as f:
+            h = SHA256.new(f.read())
+            signature = pkcs1_15.new(private_key).sign(h)
+            with open(signed_fp, "wb") as signed_file:
+                signed_file.write(signature)
+                signed_file.write(f.read())
+    else:
+        print("Error: El fichero especificado no existe")
+        sys.exit()
+
+
+def encrypt():
+    pass
+
+
+# - encriptar con clave simetrica aleatoria algoritmo AES
+# - hacer sobre digital: encriptar clave simetrica con la clave publica del receptor
+# - juntar mensaje cifrado, IV y sobre digital con el orden del enunciado
--- user.py
+++ user.py
@@ -3,7 +3,6 @@
 
 import requests
 import config
-import logging
 import os
 import sys
 from cipher import gen_id
@@ -14,7 +13,7 @@ def create_id(nombre, email):
     """Crea una nueva identidad en el sistema con el nombre e email especificados.
        Creará un par nuevo de claves publica/privada si es necesario.
     """
-    url = config.API_URL + config.ENDPOINT['register']
+    url = config.API_URL + config.ENDPOINT['register_id']
     # Generamos par de claves si no existen y obtenemos la clave publica a registrar
     gen_id()
     publ_key_path = os.path.abspath(os.path.join(config.KEYS_DIR, 'public.pem'))
@@ -37,25 +36,25 @@ def create_id(nombre, email):
 def get_public_key(user_id):
     """Devuelve la clave publica de un usuario dado su user Id (NIA).
     """
-    logging.info("Obteniendo clave publica de {}...".format(user_id))
+    print("Obteniendo clave publica de {}...".format(user_id))
     # LLamada al API Rest
     url = config.API_URL + config.ENDPOINT['publicKey']
     args = {'userID': user_id}
     headers = {"Authorization": "Bearer " + config.TOKEN}
     r = requests.post(url, json=args, headers=headers)
     if not r.ok:
-        print("\n-> Error al obtener la clave publica:\n"
-              "\t- Codigo: {}\n\t- Info: {}".format(r.json()['http_error_code'], r.json()['description']))
+        print("\n-> Error al obtener la clave publica de ID {}:\n"
+              "\t- Codigo: {}\n\t- Info: {}".format(user_id, r.json()['http_error_code'], r.json()['description']))
         sys.exit()
     else:
-        print("-> Obteniendo clave publica...OK")
+        print("-> Recuperando clave pública de ID {}...OK".format(user_id))
         return r.json()['publicKey']
 
 
 def search_id(datasearch):
     """Busca y muestra los usuarios con el nombre o email especificado en datasearch
     """
-    url = config.API_URL + config.ENDPOINT['search']
+    url = config.API_URL + config.ENDPOINT['search_id']
     # LLamada al API Rest
     headers = {"Authorization": "Bearer " + config.TOKEN}
     args = {'data_search': datasearch}
@@ -77,7 +76,7 @@ def search_id(datasearch):
 def delete_id(user_id):
     """Borra una identidad existente en el sistema. Solo podra ser borrada por el usuario que la creo
     """
-    url = config.API_URL + config.ENDPOINT['delete']
+    url = config.API_URL + config.ENDPOINT['delete_id']
     # LLamada al API Rest
     headers = {"Authorization": "Bearer " + config.TOKEN}
     args = {'userID': user_id}
