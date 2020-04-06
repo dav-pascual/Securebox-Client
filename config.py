82fc78d51375dedfaa6d98ea6eac78195f75e3ce
Javier Morales Casas <javier.moralescasas@estudiante.uam.es>
2020-04-06 04:07:28 +0200

    encrypt + enc_sign

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ TOKEN = '8eA9a7B20fcEF4dD'
 KEYS_DIR = 'keys'
 FILES_DIR = 'files'
 SIGNED_PREFIX = 'signed_'
+ENC_PREFIX = 'enc_'
 API_URL = 'https://tfg.eps.uam.es:8080/api'
 ENDPOINT = {"publicKey": '/users/getPublicKey',
             "register_id": '/users/register',
